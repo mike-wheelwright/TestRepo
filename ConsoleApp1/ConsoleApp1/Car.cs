5b0e271defe68b7a01c2a7c7312482dd1536b2c7
devmisz <mp.kolodziej@yahoo.com>
2018-10-25 11:18:36 +0200

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Car : Vehicle
+    {
+        private string color;
+
+        public string Color { get; set; }
+
+        public Car(string color) => Color = color;
+
+        public Car() { }
+
+        public override void Ride() => Console.WriteLine("wrooom!");
+
+
+    }
+}
